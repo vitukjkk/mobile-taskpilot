01ea1e3d639a9f9545f9b3506ba105f1864c4218
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-24 10:03:08 -0300

    feat: terminando styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from "react-native";
+// ELEMENTOS
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
+// COMPONENTS
+import { Tasks } from "../../components/Tasks";
+
+// STYLES
 import { styles } from "./styles";
 
 export function Home() {
     return (
         <View style={styles.container}>
-            <Text style={{color: 'white'}}>Home</Text>
+            <View>
+                <Text style={styles.title}>TaskPilot</Text>
+                <Text style={styles.subtitle}>bem-vindo ao TaskPilot, seu app de gerenciamento de tarefas</Text>
+            </View>
+
+            <View>
+                <Text style={styles.contentTitle}>Minhas tarefas</Text>
+                <Text style={styles.content}>aqui estão suas tarefas separadas por categorias</Text>
+                <Tasks />
+            </View>
+
+            <Text style={styles.contentTitle}>Categorias</Text>
+            
+            <View>
+                <Text style={styles.contentTitle}>Adicionar tarefa</Text>
+                <Text style={styles.content}>Preencha os campos e adicione uma nova tarefa</Text>
+                <TextInput 
+                    style={styles.inputContent} 
+                    placeholder="Digite o nome da tarefa"
+                    placeholderTextColor={'white'} />
+                <TextInput 
+                    style={styles.inputContent} 
+                    placeholder="Digite a descrição da tarefa"
+                    placeholderTextColor={'white'} />
+                <TextInput 
+                    style={styles.inputContent}
+                    placeholder="Digite a categoria da tarefa"
+                    placeholderTextColor={'white'} />
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Adicionar tarefa</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,102 @@
 import { StyleSheet } from "react-native";
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
+const baseStyle = StyleSheet.create({
+    
+    mainTextColor: {
+        color: "white",
+    },
+
+    mainContrastTextColor: {
+        color: "#c3c3c3",
+    },
+
+    backgroundInput: {
+        borderWidth: 1,
+        borderColor: "gray",
+        borderRadius: 5,
+        backgroundColor: "black",
+    }
+});
+
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+
+        paddingVertical: 20,
+        paddingHorizontal: 20,
         paddingTop: getStatusBarHeight(),
+        
         backgroundColor: "black",
-    }
+
+    },
+    title: {
+        ...baseStyle.mainTextColor,
+        fontSize: 32,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    subtitle: {
+        ...baseStyle.mainTextColor,
+        fontSize: 16,
+        textAlign: "center",
+    },
+    contentTitle: {
+        ...baseStyle.mainTextColor,
+        fontSize: 24,
+        fontWeight: "bold",
+        paddingVertical: 20,
+    },
+    contentSubTitle: {
+        ...baseStyle.mainTextColor,
+        fontSize: 20,
+        fontWeight: "bold",
+        paddingVertical: 10,
+    },
+    content: {
+        ...baseStyle.mainContrastTextColor,
+        fontSize: 16,
+    },
+    inputContent: {
+        ...baseStyle.backgroundInput,
+        ...baseStyle.mainTextColor,
+        width: "100%",
+        fontSize: 16,
+        padding: 10,
+        marginVertical: 10,
+    },
+
+    button: {
+        backgroundColor: "gray",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+    },
+    buttonText: {
+        ...baseStyle.mainTextColor,
+        fontSize: 16,
+    },
+});
+
+export const taskStyle = StyleSheet.create({
+    tasksContainer: {
+        padding: 10,
+        marginVertical: 10,
+        borderWidth: 1,
+        borderColor: "gray",
+        borderRadius: 5,
+        backgroundColor: "#333",
+    },
+    contentSubTitle: {
+        ...baseStyle.mainTextColor,
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    content: {
+        ...baseStyle.mainTextColor,
+        fontSize: 16,
+        textAlign: "center",
+
+    },
 });
